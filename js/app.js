065e8cc8bdd41a46770106dca96dc6bfacbe7495
Laurent Meirlaen <burner@live.be>
2016-11-11 15:40:35 +0100

    update results

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ angular.module('sortApp', [])
 	$scope.sortReverse  = true;  // set the default sort order
   
 	$scope.players = [
-		{ name: 'Valentin', matches: 5, goals: 9 },
-		{ name: 'Sylvain', matches: 6, goals: 3 },
-		{ name: 'Victor', matches: 7, goals: 5 },
-		{ name: 'PF', matches: 6, goals: 4 },
-		{ name: 'Gauthier', matches: 6, goals: 1 },
+		{ name: 'Valentin', matches: 6, goals: 10 },
+		{ name: 'Sylvain', matches: 7, goals: 3 },
+		{ name: 'Victor', matches: 8, goals: 6 },
+		{ name: 'PF', matches: 7, goals: 5 },
+		{ name: 'Gauthier', matches: 7, goals: 1 },
 		{ name: 'Thibaut', matches: 1, goals: 0 },
 		{ name: 'Nicolas', matches: 0, goals: 0 },
 		{ name: 'Pierre', matches: 6, goals: 3 },
 		{ name: 'Sevan', matches: 3, goals: 0 },
-		{ name: 'Laurent', matches: 5, goals: 0 },
+		{ name: 'Laurent', matches: 6, goals: 0 },
 		{ name: 'Emeric', matches: 4, goals: 4 }
 	];
 	
