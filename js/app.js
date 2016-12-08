03dfd5d1c54f9d6851cd82b0954d2369cc326f8e
Laurent Meirlaen <burner@live.be>
2016-12-08 19:25:44 +0100

    update results

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ angular.module('sortApp', [])
 	$scope.players = [
 		{ name: 'Valentin', matches: 8, goals: 13 },
 		{ name: 'Sylvain', matches: 7, goals: 3 },
-		{ name: 'Victor', matches: 10, goals: 7 },
-		{ name: 'PF', matches: 8, goals: 6 },
-		{ name: 'Gauthier', matches: 9, goals: 2 },
+		{ name: 'Victor', matches: 11, goals: 8 },
+		{ name: 'PF', matches: 9, goals: 9 },
+		{ name: 'Gauthier', matches: 10, goals: 2 },
 		{ name: 'Thibaut', matches: 1, goals: 0 },
 		{ name: 'Nicolas', matches: 0, goals: 0 },
 		{ name: 'Pierre', matches: 7, goals: 6 },
-		{ name: 'Sevan', matches: 5, goals: 1 },
-		{ name: 'Laurent', matches: 8, goals: 1 },
+		{ name: 'Sevan', matches: 6, goals: 1 },
+		{ name: 'Laurent', matches: 9, goals: 3 },
 		{ name: 'Emeric', matches: 4, goals: 4 }
 	];
 	
